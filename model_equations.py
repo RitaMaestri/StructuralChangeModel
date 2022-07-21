fd31684321f7ec78847479751d77293e6078df53
RitaMaestri <rita.maestri95@gmail.com>
2022-07-21 15:35:40 +0200

    non converging least square optimization for the minimal model ( no intermediate goods)

--- model_equations.py
+++ model_equations.py
@@ -1,61 +1,101 @@
 #equations cge
 import numpy as np
 
-def eqKLj(KLj, bKLj, Lj, Kj, alphaLj, alphaKj):
-    
-    
-    
-    zero = KLj - bKLj * np.power(Lj,alphaLj) * np.power(Kj,alphaKj)
-    
-    return zero
+def shapeError(*args):
+    if not all(x.shape[0] == args[0].shape[0] for x in args):
+        raise Exception("Wrong shape")
 
-#same equation for Lj and Kj (Factors) Fbar is the complementary factor (K if F=L and viceversa)
 
-def eqFj(Fj,GDP,pFbar,Fbar,F,KLj,pKLj,alphaFj):
+def notScalarError(*args):
+    if not all(np.isscalar(x) for x in args):
+        raise Exception("Scalar needed")
+
+
+def notArrayError(*args):
+
+    if not all(isinstance(i, np.ndarray) for i in args):
+        raise Exception("Array needed")
+
+
+#EQUATIONS
+
+def eqKLj(KLj, bKL, Lj, Kj, alphaLj, alphaKj):
+    #print(bKL)    notArrayError(KLj, bKL, Lj, Kj, alphaLj, alphaKj)
+    shapeError(KLj, Lj, Kj, alphaLj, alphaKj)
+    bKL=bKL.item()
+
+    zero = KLj - bKL * np.power(Lj,alphaLj) * np.power(Kj,alphaKj)
     
-    zero= Fj-(np.prod(np.vstack([pKLj,KLj,alphaFj]), axis=0)*F)/(GDP-pFbar*Fbar)
+    return zero 
+
+# same equation for Lj and Kj (Factors)
+
+def eqFj(Fj,pF,KLj,pKLj,alphaFj):
+    
+   # type and shape checks
+    
+    notArrayError(Fj,pF,KLj,pKLj,alphaFj)
+    pF=pF.item()
+    notScalarError(pF)
+    shapeError(Fj,KLj,pKLj,alphaFj)
+    
+    # final result
+    
+    zero= Fj-(np.prod(np.vstack([pKLj,KLj,alphaFj]), axis=0))/pF
     
     return zero
 
 
 def eqYij(Yij,aYij,Yj):
+    notArrayError(Yij,aYij,Yj)
+    shapeError(Yij,aYij,Yj)
     
     Yjd=np.diag(Yj)
     
     zero=Yij-np.dot(aYij,Yjd)
+    
     #convert matrix to vector
     zero=zero.flatten()
     
     return zero
 
 
-
 def eqKL(KLj,aKLj,Yj):
+    notArrayError(KLj,aKLj,Yj)
+    shapeError(KLj,aKLj,Yj)
     
-    zero=KLj-np.multiply(aKLj,Yj)
+    zero=KLj- np.multiply(aKLj,Yj)
     
     return zero
 
 
-
 def eqpYj(pYj,aKLj,pKLj,aYij):
+    notArrayError(pYj,aKLj,pKLj,aYij)
+    shapeError(pYj,aKLj,pKLj,aYij)
     
     pYjd=np.diag(pYj)
-
+    
     zero=pYj - (np.multiply(aKLj,pKLj) + np.dot(pYjd,aYij).sum(axis=0))
     
     return zero
 
 
-def eqCj(Cj,alphaCj,pCj,GDP):
+def eqCj(Cj,alphaCj,pCj,pL,L,pK,K):
+    notArrayError(Cj,alphaCj,pCj,pL,L,pK,K)
+    shapeError(Cj,alphaCj,pCj)
     
-    zero=Cj-alphaCj*GDP/pCj
+    L,pL,K,pK=L.item(),pL.item(),K.item(),pK.item()
+
+    notScalarError(L,pL,K,pK)
+    #("Cj=", Cj, "alphaCj= ", alphaCj, "pL*L+pK*K=", pL*L+pK*K, "pCj", pCj  )
+    zero= Cj - alphaCj * (pL*L+pK*K) / pCj
     
     return zero
     
-
     
 def eqYj(Yj,Cj,Yij):
+    notArrayError(Yj,Cj,Yij)
+    shapeError(Yj,Cj,Yij)
     
     zero = Yj-(Cj + Yij.sum(axis=1))
     
@@ -63,20 +103,31 @@ def eqYj(Yj,Cj,Yij):
 
 
 #same equation for L and K
+
 def eqF(F,Fj):
-    
+    notArrayError(F,Fj)
+
+    F=F.item()
+    #print("F=", F, "Fj=",Fj, " sum(Fj)=", sum(Fj))
     zero= F-sum(Fj)
     
     return zero
 
 
 def eqpCj(pCj,pYj):
-    
+    notArrayError(pCj,pYj)
+    shapeError(pCj,pYj)
+     
     zero=pCj-pYj
     
     return zero
 
 def eqGDP(GDP,pL,L,pK,K):
+    GDP=GDP.item()
+    pL=pL.item()
+    L=L.item()
+    pK=pK.item()
+    K=K.item()
     
     zero= GDP-( pL*L+ pK*K)
     
--- simulation.py
+++ simulation.py
@@ -3,6 +3,8 @@ import numpy as np
 import sys
 from math import sqrt
 
+
+
 sys.path.append('/home/rita/Documents/Stage/Code')
 import model_equations as eq
 
@@ -18,6 +20,11 @@ def to_dict(vec, dvec):  #takes array, returns dict of arrays (of equal dimensio
     return dict(zip(keys, vec))
 
 
+def to_dict_jac(vec, dvec):  #takes array, returns dict of arrays (of equal dimensions and keys as dvec)
+    keys=dvec.keys()
+    vec=int(vec)
+    return dict(zip(keys, vec))
+
 def to_array(dvar):  #takes dict returns array
     var=[np.array(dvar[k]) for k in dvar.keys()]
     return np.array(np.hstack([var[i].flatten() for i in range(len(var))]))
@@ -25,28 +32,42 @@ def to_array(dvar):  #takes dict returns array
 
 def system(var, par):
     
+
     d = {**var, **par}
     
+
     return np.hstack([
-        eq.eqKLj(d['KLj'], d['bKLj'], d['Lj'], d['Kj'], d['alphaLj'], d['alphaKj']),
-        eq.eqFj(d['Lj'],d['GDP'],d['pK'],d['K'],d['L'],d['KLj'],d['pKLj'],d['alphaLj']),
-        eq.eqFj(d['Kj'],d['GDP'],d['pL'],d['L'],d['K'],d['KLj'],d['pKLj'],d['alphaKj']),
-        eq.eqYij(d['Yij'],d['aYij'],d['Yj']),
-        eq.eqKL(d['KLj'],d['aKLj'],d['Yj']),
-        eq.eqpYj(d['pYj'],d['aKLj'],d['pKLj'],d['aYij']),
-        eq.eqCj(d['Cj'],d['alphaCj'],d['pCj'],d['GDP']),
-        eq.eqYj(d['Yj'],d['Cj'],d['Yij']),
-        eq.eqF(d['L'],d['Lj']),
-        eq.eqF(d['K'],d['Kj']),
-        eq.eqpCj(d['pCj'],d['pYj'])
+        #eq.eqKLj(d['KLj'], d['bKL'], d['Lj'], d['Kj'], d['alphaLj'], d['alphaKj']),
+        eq.eqKLj(KLj=d['Cj'], bKL=d['bKL'], Lj=d['Lj'], Kj=d['Kj'], alphaLj=d['alphaLj'], alphaKj=d['alphaKj']),
+        eq.eqFj(Fj=d['Lj'],pF=d['pL'],KLj=d['Cj'],pKLj=d['pKLj'],alphaFj=d['alphaLj']),
+        eq.eqFj(Fj=d['Kj'],pF=d['pK'],KLj=d['Cj'],pKLj=d['pKLj'],alphaFj=d['alphaKj']),
+        # eq.eqFj(d['Lj'],d['pL'],d['KLj'],d['pKLj'],d['alphaLj']),
+        # eq.eqFj(d['Kj'],d['pK'],d['KLj'],d['pKLj'],d['alphaKj']),
+        
+        #eq.eqYij(d['Yij'],d['aYij'],d['Yj']),
+        #eq.eqKL(d['KLj'],d['aKLj'],d['Yj']),
+        #eq.eqpYj(d['pYj'],d['aKLj'],d['pKLj'],d['aYij']),
+        eq.eqCj(Cj=d['Cj'],alphaCj=d['alphaCj'],pCj=d['pCj'],pL=d['pL'],L=d['L'],pK=d['pK'],K=d['K']),
+        #eq.eqYj(d['Yj'],d['Cj'],d['Yij']),
+        eq.eqF(F=d['L'],Fj=d['Lj']),
+        eq.eqF(F=d['K'],Fj=d['Kj']),
+        eq.eqpCj(pCj=d['pCj'],pYj=d['pKLj']),
+       # eq.eqpCj(d['pCj'],d['pYj']),
+        #eq.eqGDP(d['GDP'], d['pL'], d['L'], d['pK'], d['K'])
         ])
 
 
 def dict_least_squares(f, dvar, dpar):
     result = optimize.least_squares(
-        lambda x,y: f(to_dict(x,dvar), to_dict(y,dpar)), # wrap the argument in a dict
+        lambda x,y: f(to_dict(x,dvar), to_dict(y,dpar)),# wrap the argument in a dict
         to_array(dvar), # unwrap the initial dictionary
-        args= list([to_array(dpar)])
+        bounds=(0,[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,1,1,np.inf,np.inf,]),
+        args= list([to_array(dpar)],),
+        ftol=None,
+        xtol=None,
+        gtol=10e-15,
+        #verbose=2,
+        x_scale=np.array([1,100,100,100,100,100,100,0.1,0.1,1,1]),
     )
     return result;
 
@@ -60,58 +81,65 @@ def dict_fsolve(f, dvar, dpar):
     return result;
 
 
-variables = {'pL': np.array([59057.25132478]),
-  'pK': np.array([-38341.90453179]),
-  'KLj': np.array([ 0.36712262, 14.05476505]),
-  'Lj': np.array([-0.46674462,  3.50519154]),
-  'Kj': np.array([ 1.12095355, -0.21571321]),
-  'pKLj': np.array([-18116.96037197,   1565.67981374]),
-  'Yj': np.array([1.30167461, 7.02791538]),
-  'pYj': np.array([1361.56045735, 8095.77017803]),
-  'Cj': np.array([-0.84879547,  0.0140812 ]),
-  'pCj': np.array([1361.56046012, 8095.77017959]),
-  'aYij': np.array([[0.76367541, 0.28395635],
-        [2.27757584, 0.56545415]])}
 
-
-# variables = { 
-#     'pL':np.array([1]),
-#     'pK':np.array([2]),
-#     'KLj':np.array([80,80]),
-#     'Lj':np.array([1,1]),
-#     'Kj':np.array([3,15]),
-#     'pKLj':np.array([15,15]),
-#     'Yj':np.array([1,2]),
-#     'pYj':np.array([1,4]),
-#     'Cj':np.array([2,1]),
-#     'pCj':np.array([1,3]),
-#     'aYij':np.array([[1,0],[1,1]])
-#     }
-    
-parameters= {
-    'GDP':np.array([1]),
-    'K':np.array([2]),
-    'L':np.array([3]),
-    'bKLj':np.array([1,2]),
-    'alphaKj':np.array([1,2]),
-    'alphaLj':np.array([3,4]),
-    'alphaCj':np.array([1,2]),
-    'aKLj':np.array([1,2]),
-    'Yij':np.array([[1,2],[3,4]])
+variables = {
+    'pK':np.array([.1]),
+    'Lj':np.array([200,20000]),
+    'Kj':np.array([500,20000]),
+    'pKLj':np.array([1,1]),
+    'alphaCj':np.array([0.9,0.1]),
+    'pCj':np.array([4,20]),
     }
 
 
-#add the lengths of variables to the parameters
+parameters= {
+    'pL':np.array([1]),
+    'K':np.array([334]),
+    'L':np.array([183]),
+    'bKL':np.array([0.8]),
+    'alphaKj':np.array([.5,.5]),
+    'alphaLj':np.array([.5,.5]),
+    'Cj':np.array([438,293]),
+    }
 
-solLS= dict_least_squares(system, variables, parameters).x
-solFS= dict_fsolve(system, variables, parameters)
+
+
+# variables = {
+#     'pK':np.array([4]),
+#     'KLj':np.array([10,30]),
+#     'Lj':np.array([200,2000]),
+#     'Kj':np.array([4000,11]),
+#     'pKLj':np.array([345,789]),
+#     'Yj':np.array([11,12]),
+#     'pYj':np.array([4,5]),
+#     'alphaCj':np.array([0.9,0.1]),
+#     'pCj':np.array([4,20]),
+#     'aYij':np.array([[0.2,0.1],[0.4,0.5]])
+#     }
+    
+
+# parameters= {
+#     'pL':np.array([1]),
+#     'K':np.array([334]),
+#     'L':np.array([183]),
+#     'bKL':np.array([0.8]),
+#     'alphaKj':np.array([.5,.5]),
+#     'alphaLj':np.array([.5,.5]),
+#     'Cj':np.array([438.63792689187,78.3620731081594]),
+#     'aKLj':np.array([0.5,0.5]),
+#     'Yij':np.array([[148.892198751339,57.469874356791],[202.669132341745,65.9687945500961]])
+#     }
+
+
+
+sol= dict_least_squares(system, variables, parameters)
+solLS=sol.x
+#solFS= dict_fsolve(system, variables, parameters)
 
 print(system(to_dict(solLS,variables), parameters))
 #print(system(to_dict(solFS,variables), parameters))
 
-#test radom equations
 
 d = {**to_dict(solLS, variables), **parameters}
-#print(eq.eqCj(d["Cj"],d["alphaCj"],d["pCj"],d["GDP"]))
 
 
