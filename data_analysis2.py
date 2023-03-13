d862366dd1820afd39fcb090de358a53dff4ea7a
RitaMaestri <rita.maestri95@gmail.com>
2023-03-13 18:46:35 +0100

    data analysis

--- data_analysis2.py
+++ data_analysis2.py
@@ -7,6 +7,7 @@ import import_csv as imp
 import sys
 from simple_calibration import N
 import matplotlib.colors as mcolors
+from scipy import stats
 
 sectors_names_eng=[
     "AGRICULTURE",
@@ -48,122 +49,133 @@ sectors_names_eng=[
     "HOUSEHOLD ACTIVITIES"
     ]
 
+
+cmap=["#E06969",
+"#E80000",
+"#B30000",
+"#FFA64D",
+"#F27900",
+"#B35900",
+"#FFFF73",
+"#BFBF00",
+"#7A7A00",
+"#B9FF73",
+"#6CD900",
+"#3D7A00",
+"#54F0A2",
+"#00BF60",
+"#00572B",
+"#8FFFFF",
+"#19C6FF",
+"#007D7D",
+"#73B9FF",
+"#0060BF",
+"#003469",
+"#8C8CFF",
+"#5959FF",
+"#0000FF",
+"#000069",
+"#B973FF",
+"#6300C7",
+"#340069",
+"#FF00FF",
+"#B00CB0",
+"#5C005C",
+"#9C004E",
+"#FF1F8F",
+"#FF91C8",
+"#000000",
+"#545454",
+"#9294A1"]
+
+my_cmap=mcolors.ListedColormap(cmap)
+
 name = imp.sectors
 
-df = pd.read_csv("results/johansen2015-2050exoKnext(02-03-2023_18:37).csv", index_col=0)
+df = pd.read_csv("results/johansen2015-2050exoKnext(02-03-2023_18:37).csv")
 df.rename(columns={df.columns[0]: 'variable'},inplace=True)
 
 df.loc[ df['variable'] == "L" ].drop("variable", axis=1)
 a=df.loc[ df['variable'] == "bKL" ].drop("variable", axis=1)
 
 
-diff= False
-var='KLj'
-#"p","q","pq"
-pq="q"
 
 
+#pq = "p","q","pq"  
+def extract_var_df(var, pq):
+    P=df.loc[ df['variable'] == "p"+var ].reset_index(drop=True).drop("variable", axis=1)
+    Q=df.loc[ df['variable'] == var ].reset_index(drop=True).drop("variable", axis=1)
 
-
-
-P=df.loc[ df['variable'] == "p"+var ].reset_index(drop=True).drop("variable", axis=1)
-Q=df.loc[ df['variable'] == var ].reset_index(drop=True).drop("variable", axis=1)
-
-
-if pq=="pq":
-    VA = P*Q
-elif pq == "p":
-    VA = P
-elif pq == "q":
-    VA= Q
-else:
-    print("wrong pq!")
-    sys.exit()
-
-VA.insert(0, "Name", sectors_names_eng, True)
-
-# start_range=sectors_names_eng.index("PHARMACEUTICAL IND.")
-# this_range=range(N*start_range,N*start_range+N)
-
-# var="Yij"
-
-# Q=df.loc[ df['variable'] == "Yij" ].reset_index(drop=True).drop("variable", axis=1).loc[this_range]
-
-# VA=Q
-
-fig = plt.figure(figsize=(20,15))
-ax = fig.add_subplot(111)
-
-x=np.array(VA.columns[2:]).astype('int')
-
-y0= x[1].astype(str)
-yL=x[-1].astype(str)
-
-rank= np.argsort(VA[y0].values/VA[yL].values)
-index_side_writing=np.hstack([rank[:3],rank[-3:]])
-
-for j in VA.index:
-    y=np.array(VA.loc[j])[2:].astype('float')
-    y= y/y[0]-shift(y/y[0], 1, cval=np.NaN) if diff else y/y[0]
-    lab=VA.loc[j][0]
-    ax.plot(x[0:],y[0:] , label = lab)
-    if j in index_side_writing:
-        print(j)
-        print(VA.loc[j][0])
-        ax.annotate(xy=(x[-1],y[-1]), xytext=(5,0), textcoords='offset points', text=VA.loc[j][0], va='center')
-
+    if pq=="pq":
+        var_df = P*Q
+    elif pq == "p":
+        var_df = P
+    elif pq == "q":
+        var_df= Q
+    else:
+        print("wrong pq!")
+        sys.exit()
+    #print(var_df)
+    if len(var_df.index)>1:
+        var_df.insert(0, "Name", sectors_names_eng, True) 
+    return var_df 
     
     
-colors1=plt.cm.Accent(np.linspace(0., 1, 128))
-colors2=plt.cm.tab10(np.linspace(0., 1, 128))
-colors3=plt.cm.Set3(np.linspace(0.1, 1, 128))
-colors4=plt.cm.gist_ncar(np.linspace(0., .8, 128))
-mycolors = np.vstack((colors1, colors2, colors3, colors4))
-colormap = mcolors.LinearSegmentedColormap.from_list('my_colormap', mycolors)
 
 
- #nipy_spectral, Set1,Paired   
-colors = [colormap(i) for i in np.linspace(0, 1,len(ax.lines))]
-for i,j in enumerate(ax.lines):
-    j.set_color(colors[i])
+def plot_varj_evol(var, pq, diff=False):
     
-# Shrink current axis's height by 10% on the bottom
-# Shrink current axis by 20%
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-# # Put a legend to the right of the current axis
-ax.legend(loc='center left', bbox_to_anchor=(1.16, 0.5), prop={'size': 16})
-
-if pq=="pq":
-    plt.title("p"+var+var)
-elif pq == "p":
-    plt.title("p"+var)
-elif pq == "q":
-    plt.title(var)
-plt.xlim(2014.99,2050.01)
-
-plt.show()
-
-
-
-
-
-for i in np.split(VA.index, [9,18,27]):
-    for j in i:
-        x=np.array(VA.columns[1:]).astype('int')
-        y=np.array(VA.loc[j])[1:].astype('float')
+    var_df=extract_var_df(var, pq)
+    
+    fig = plt.figure(figsize=(20,15))
+    ax = fig.add_subplot(111)
+    
+    x=np.array(var_df.columns[1:]).astype('int')
+    
+    y0= x[0].astype(str)
+    yL= x[-1].astype(str)
+    
+    
+    relative_values=var_df[yL].values/var_df[y0].values
+    nans=np.argwhere(np.isnan(relative_values)).flatten()
+    #nansX=np.argwhere(np.array(df["2050"].loc[ df['variable'] == "Xj" ]==0)).flatten()
+    rank= np.argsort(-relative_values)
+    rank=rank[~np.in1d(rank,nans)]
+    index_side_writing=np.hstack([rank[:3],rank[-3:]])
+    
+    
+    for j in rank:
+        y=np.array(var_df.loc[j])[1:].astype('float')
         y= y/y[0]-shift(y/y[0], 1, cval=np.NaN) if diff else y/y[0]
-        plt.plot(x[0:],y[0:] , label = VA.loc[j][0])
-    ax = plt.subplot(111)
+        lab=var_df.loc[j][0]
+        ax.plot(x[0:],y[0:] , label = lab)
+        if j in index_side_writing:
+            ax.annotate(xy=(x[-1],y[-1]), xytext=(5,0), textcoords='offset points', text=var_df.loc[j][0], va='center')
+    
+        
+        
+    # colors1=plt.cm.Accent(np.linspace(0., 1, 128))
+    # colors2=plt.cm.tab10(np.linspace(0., 1, 128))
+    # colors3=plt.cm.Set3(np.linspace(0.1, 1, 128))
+    # colors4=plt.cm.gist_ncar(np.linspace(0., .8, 128))
+    # mycolors = np.vstack((colors1, colors2, colors3, colors4))
+    # colormap = mcolors.LinearSegmentedColormap.from_list('my_colormap', mycolors)
+    
+    
+    #  #nipy_spectral, Set1,Paired   
+    # colors = [colormap(i) for i in np.linspace(0, 1,len(ax.lines))]
+    
+    for i,j in enumerate(ax.lines):
+        j.set_color(my_cmap.colors[i])
+        
     
     # Shrink current axis's height by 10% on the bottom
     # Shrink current axis by 20%
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
     
-    # Put a legend to the right of the current axis
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    # # Put a legend to the right of the current axis
+    ax.legend(loc='center left', bbox_to_anchor=(1.16, 0.5), prop={'size': 16})
     
     if pq=="pq":
         plt.title("p"+var+var)
@@ -171,17 +183,51 @@ for i in np.split(VA.index, [9,18,27]):
         plt.title("p"+var)
     elif pq == "q":
         plt.title(var)
+    plt.xlim(2014.99,2050.01)
     
+    plt.title("Value added by sector", fontsize = 25)
+    plt.xlabel("Year",fontsize = 15)
+    plt.ylabel("Relative change with respect to year 2015", fontsize = 15)
 
+    
     plt.show()
+
+plot_varj_evol(var="KLj", pq="q", diff=False)
+
+
+def plot_splitted_evolutions(var, pq, diff):
+    
+    var_df=extract_var_df(var, pq)
+    for i in np.split(var_df.index, [9,18,27]):
+        for j in i:
+            x=np.array(var_df.columns[1:]).astype('int')
+            y=np.array(var_df.loc[j])[1:].astype('float')
+            y= y/y[0]-shift(y/y[0], 1, cval=np.NaN) if diff else y/y[0]
+            plt.plot(x[0:],y[0:] , label = var_df.loc[j][0])
+        ax = plt.subplot(111)
+        
+        # Shrink current axis's height by 10% on the bottom
+        # Shrink current axis by 20%
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        
+        # Put a legend to the right of the current axis
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+        
+        if pq=="pq":
+            plt.title("p"+var+var)
+        elif pq == "p":
+            plt.title("p"+var)
+        elif pq == "q":
+            plt.title(var)
+        
+        plt.show()
 
 
 
 pharma_ind=sectors_names_eng.index("PHARMACEUTICAL IND.")
 agri_ind=sectors_names_eng.index("AGRICULTURE")
     
-#par_keys = ['aKLj', 'alphaCj', 'alphaDj','alphaGj','alphaIj', 'alphaKj', 
-#            'alphaLj','alphaXj','betaDj','betaMj', 'bKLj', 'tauYj', 'tauSj']
 
 par_keys = ['tauSj', 'tauYj', 'pXj', 'bKLj', 'alphaKj', 'alphaLj', 'aKLj', 'alphaCj', 
 'alphaGj', 'alphaIj', 'alphaXj', 'alphaDj', 'betaDj', 'betaMj', 
@@ -194,38 +240,52 @@ for i in par_keys:
     print(i,36-18-np.where(order==pharma_ind)[0].item(),"\n")
 
 
-#plot capital and labour prices
-pL=df.loc[ df['variable'] == "pL" ].reset_index(drop=True).drop("variable", axis=1)
-pK=df.loc[ df['variable'] == "pK" ].reset_index(drop=True).drop("variable", axis=1)
-
-x=np.array(VA.columns[1:]).astype('int')
-y=np.array(pK)[0].astype('float')
-y= y-shift(y, 1, cval=np.NaN) if diff else y
-plt.plot(x[1:],y[1:] , label = "pL")
-plt.title("pK")
-plt.show()
+################# plot capital and labour prices ###############################
 
 
 
-#plot GDP growth, capital and labour growth
+
+def plot_variable_1D(var_df, title, diff=False):
+    fig = plt.figure(figsize=(15,10))
+    x=np.array(var_df.columns).astype('int')
+    y=np.array(var_df)[0].astype('float')
+    y= y-shift(y, 1, cval=np.NaN) if diff else y
+    plt.plot(x[1:],y[1:] , label = title)
+    plt.title(title, fontsize = 35)
+    plt.xlabel("Year",fontsize = 20)
+    plt.ylabel("Relative change with respect to year 2015", fontsize = 20)
+
+    plt.show()
+    
+variable= extract_var_df("GDPreal", "q")
+plot_variable_1D(variable, "Real GDP", diff=False)
+###############################################################################
+
+
+
+
+
+################# plot GDP growth, capital and labour growth #######################
 L=np.array(df.loc[(df['variable'] == "L")].iloc[:,1:])[0].astype("float")
 bKL=np.array(df.loc[(df['variable'] == "bKL")].iloc[:,1:])[0].astype("float")
 LbKL=L*bKL
 K=np.array(df.loc[(df['variable'] == "K")].iloc[:,1:])[0].astype("float")
 GDPreal=np.array(df.loc[(df['variable'] == "GDPreal")].iloc[:,1:])[0].astype("float")
 
-fig = plt.figure(figsize=(15,10))
+fig = plt.figure(figsize=(20,15))
 ax = fig.add_subplot(111)
 x=np.array(df.columns[1:]).astype('int')
-y= LbKL/LbKL[0]-shift(LbKL/LbKL[0], 1, cval=np.NaN) if diff else LbKL/LbKL[0]
-plt.plot(x,y,label = "$L$ x $b_{KL}$")
-y= GDPreal/GDPreal[0]-shift(GDPreal/GDPreal[0], 1, cval=np.NaN) if diff else GDPreal/GDPreal[0]
-plt.plot(x,y,label = "$GDPreal$")
-y= K/K[0]-shift(K/K[0], 1, cval=np.NaN) if diff else K/K[0]
-plt.plot(x,y,label = "$K$")
+y=  GDPreal/GDPreal[0]
+plt.plot(x,y,label = "$GDP$")
+y=  LbKL/LbKL[0]
+plt.plot(x,y,label = "$Labour$")
 
+y=  K/K[0]
+plt.plot(x,y,label = "$Capital$")
 
-
+# colors= ["#06646E","#0DABBD","#56C9D6"]
+# for i,j in enumerate(ax.lines):
+#     j.set_color(colors[i])
 # Shrink current axis's height by 10% on the bottom
 # Shrink current axis by 20%
 box = ax.get_position()
@@ -233,32 +293,60 @@ ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 
 # Put a legend to the right of the current axis
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop={'size': 16})
+plt.title("Evolution of the parameters from the coupled IAM", fontsize = 25)
+plt.xlabel("Year",fontsize = 15)
+plt.ylabel("Relative change with respect to year 2015", fontsize = 15)
+
 plt.show()
+###############################################################################
 
 
 
 
+######################### numerical analysis of KLj2050 extremes ###############
+def order_variablesj (var, relative=False):
+    array2015=np.array(df["2016"].loc[ df['variable'] == var ]) 
+    array2050=np.array(df["2050"].loc[ df['variable'] == var ])
+    array= array2050/array2015 if relative else array2050  
+    return(pd.Series(array, index =sectors_names_eng).sort_values())
 
-# x=np.array(df.columns[1:]).astype('int')
-# y= L
-# plt.plot(x,y,label = "L")
-# y= K
-# plt.plot(x,y,label = "K")
+def value(var, sec="PHARMACEUTICAL IND."):
+    array2050=np.array(df["2050"].loc[ df['variable'] == var ])
+    series=pd.Series(array2050, index =sectors_names_eng).sort_values()
+    return series[sec]
 
-# ax = plt.subplot(111)
+print(value("KLj", "MACHINES"))
 
-# # Shrink current axis's height by 10% on the bottom
-# # Shrink current axis by 20%
-# box = ax.get_position()
-# ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+par_keys = ['tauSj', 'tauYj', 'pXj', 'bKLj', 'alphaKj', 'alphaLj', 'aKLj', 'alphaCj', 
+'alphaGj', 'alphaIj', 'alphaXj', 'alphaDj', 'betaDj', 'betaMj', 
+'sigmaXj', 'sigmaSj']
 
-# # Put a legend to the right of the current axis
-# ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-# plt.title("L & K")
-# plt.show()
+nansX=np.argwhere(np.array(df["2050"].loc[ df['variable'] == "Xj" ]==0)).flatten()
 
 
+plot_varj_evol(var="KLj", pq="q", diff=False)
 
+order_variablesj("alphaXj")#.drop([sectors_names_eng[i] for i in nans])
+
+
+def function_pD(pY,sigmaY,alphaX,pX,alphaD):
+    Ysigma=1-sigmaY
+    pD=(((pY**Ysigma)-(alphaX**sigmaY)*(pX**Ysigma)) / (alphaD**sigmaY))**(1/Ysigma)
+    return pD
+
+value("pDj", "AGRICULTURE")
+
+print(function_pD(pY=value("pYj"),
+                  sigmaY=value("sigmaXj"),
+                  alphaX=value("alphaXj","AGRICULTURE"),
+                  pX=value("pXj"),
+                  alphaD=value("alphaDj")))
+
+
+plot_varj_evol(var="KLj", pq="q", diff=False)
+
+
+###########
 
 
 
